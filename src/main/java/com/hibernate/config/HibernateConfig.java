15739b8e2594214051e9a93a66d25714b54819b6
RajalakshmiElumalaiCts <rajalakshmi.personal@gmail.com>
2017-03-01 07:46:29 -0600

    config

--- src/main/java/com/hibernate/config/HibernateConfig.java
+++ src/main/java/com/hibernate/config/HibernateConfig.java
@@ -4,13 +4,18 @@ import java.util.Properties;
 
 import javax.sql.DataSource;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.orm.hibernate3.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
@@ -20,7 +25,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class HibernateConfig {
 	
 	@Autowired
-	private Environment environment;
+	private Environment env;
 	
 	@Bean
 	public LocalSessionFactoryBean sessionFactory(){
@@ -32,13 +37,41 @@ public class HibernateConfig {
 	}
 
 	private Properties getHibernateProperties() {
-		// TODO Auto-generated method stub
+		Properties props = new Properties();
+		props.setProperty("hibernate.connection.hbm2ddl.auto",
+	              env.getProperties().getProperty("hibernate.connection.hbm2ddl.auto"));
+		props.setProperty("hibernate.connection.dialect",
+	              env.getProperties().getProperty("hibernate.connection.dialect"));
+		props.setProperty("hibernate.globally_quoted_identifiers",
+	             "true");
+		props.setProperty("hibernate.connection.show_sql",
+				 env.getProperties().getProperty("hibernate.connection.show_sql"));
+		
 		return null;
 	}
 
-	private DataSource getDatasource() {
-		// TODO Auto-generated method stub
-		return null;
+	@Bean
+	public DataSource getDatasource() {
+		BasicDataSource dataSource = new BasicDataSource();
+	      dataSource.setDriverClassName(env.getProperties().getProperty("hibernate.connection.driver_class"));
+	      dataSource.setUrl(env.getProperties().getProperty("hibernate.connection.url"));
+	      dataSource.setUsername(env.getProperties().getProperty("hibernate.connection.username"));
+	      dataSource.setPassword(env.getProperties().getProperty("hibernate.connection.password"));
+		return dataSource;
 	}
 
+	@Bean
+	   @Autowired
+	   public HibernateTransactionManager transactionManager() {
+	  
+	      HibernateTransactionManager txManager = new HibernateTransactionManager();
+	      txManager.setSessionFactory(sessionFactory().getObject());
+	 
+	      return txManager;
+	   }
+
+	@Bean
+	   public static PropertySourcesPlaceholderConfigurer PropertySourcesPlaceholderConfigurer() {
+	      return new PropertySourcesPlaceholderConfigurer();
+	   }
 }
